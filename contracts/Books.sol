3824191798dce875d4cf34e9370825de66f64aa3
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-09-28 04:16:58 -0400

    added total supply of 1000

--- contracts/Books.sol
+++ contracts/Books.sol
@@ -5,6 +5,6 @@ import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 contract BooksToken is ERC20{
 
     constructor () public ERC20 ('Books Edition 0', 'BOOKS') {
-        // _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
+        _mint(msg.sender, 1000);
     }
 }
